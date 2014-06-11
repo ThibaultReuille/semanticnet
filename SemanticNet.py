27c534e1db4c73afbe4e28d322129a2e5182196f
skyler <skyler@opendns.com>
2014-06-11 14:24:48 -0700

    Rename g to _g to indicate internal use only.

--- SemanticNet.py
+++ SemanticNet.py
@@ -19,7 +19,7 @@ class Event:
 
 class Graph:
     def __init__(self, verbose=False):
-        self.g = nx.MultiGraph()
+        self._g = nx.MultiGraph()
         self.edges = {}
 
         self.verbose = verbose
@@ -46,31 +46,31 @@ class Graph:
     def add_node(self, data):   
         id = self.create_node_uid()
         self.log("add_node " + str(data) + " = " + str(id)) 
-        self.g.add_node(id, data)
+        self._g.add_node(id, data)
         return id
 
     def add_edge(self, src, dst, data={}):
-        if self.g.has_node(src) and self.g.has_node(dst):
+        if self._g.has_node(src) and self._g.has_node(dst):
             id = self.create_edge_uid()
             self.log("add_edge " + str(src) + ", " + str(dst) + ", " + str(data) + " = " + str(id))
-            self.g.add_edge(src, dst, id, dict(chain(data.items(), {"id": id}.items())) )
-            self.edges[id] = self.g.edge[src][dst][id]
+            self._g.add_edge(src, dst, id, dict(chain(data.items(), {"id": id}.items())) )
+            self.edges[id] = self._g.edge[src][dst][id]
             return id
         else:
             raise GraphException("Node ID not found, can't create edge.")
 
     def set_node_attribute(self, id, attr_name, value):
-        if self.g.has_node(id):
+        if self._g.has_node(id):
             if attr_name in self.attr_reserved:
                 raise GraphException("Attribute {} is reserved.".format(attr_name))
 
-            self.g.node[id][attr_name] = value
+            self._g.node[id][attr_name] = value
         else:
             raise GraphException("Node id not found, can't set attribute.")
 
     def get_node_attribute(self, id, attr_name):
-        if self.g.has_node(id):
-            return self.g.node[id][attr_name]
+        if self._g.has_node(id):
+            return self._g.node[id][attr_name]
         else:
             raise GraphException("Node ID not found, can't get attribute")
 
@@ -99,14 +99,14 @@ class Graph:
         with open(filename, 'w') as outfile:
             graph = dict()
             graph["meta"] = self.meta
-            graph["nodes"] = [ dict(chain({"id": i}.items(), self.g.node[i].items())) for i in self.g.nodes() ]
+            graph["nodes"] = [ dict(chain({"id": i}.items(), self._g.node[i].items())) for i in self._g.nodes() ]
             graph["edges"] = [ dict(
                 chain(
                     { "src": i, "dst": j, "id": key}.items(),
-                    self.g.edge[i][j][key].items())
+                    self._g.edge[i][j][key].items())
                 )
-                for i, j in self.g.edges()
-                for key in self.g.edge[i][j]
+                for i, j in self._g.edges()
+                for key in self._g.edge[i][j]
             ]
             graph["timeline"] = [ [c.timecode, c.name, c.attributes] for c in self.timeline ]
             json.dump(graph, outfile)
@@ -118,13 +118,13 @@ class Graph:
             self.timeline = graph["timeline"]
 
             for node in graph["nodes"]:
-                self.g.add_node(node["id"], dict([item for item in node.items() if item[0] != 'id']))
+                self._g.add_node(node["id"], dict([item for item in node.items() if item[0] != 'id']))
 
             for edge in graph["edges"]:
-                self.g.add_edge(edge["src"], edge["dst"], dict(item for item in edge.items() if (item[0] != "src" and item[0] != "dst") ))
+                self._g.add_edge(edge["src"], edge["dst"], dict(item for item in edge.items() if (item[0] != "src" and item[0] != "dst") ))
 
-            self.last_node_id = len(self.g.nodes())
-            self.last_edge_id = len(self.g.edges())
+            self.last_node_id = len(self._g.nodes())
+            self.last_edge_id = len(self._g.edges())
 
 if __name__ == "__main__":
     print("Please import this module !")
