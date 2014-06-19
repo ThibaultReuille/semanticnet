217bcd9994f78f9921d226c2cb0a976475ca530f
skyler <skyler@opendns.com>
2014-06-19 10:17:45 -0700

    Add DiGraph class w/ fixed remove_node()

---
+++ DiGraph.py
@@ -0,0 +1,31 @@
+import networkx as nx
+from semanticnet import Graph
+
+class DiGraph(Graph):
+
+    def __init__(self, verbose=False):
+        self._g = nx.MultiDiGraph()
+        self._edges = {}
+        self.meta = {}
+        self.timeline = []
+
+        self.verbose = verbose
+        self.attr_reserved = ["id", "src", "dst"]
+
+    def remove_node(self, id_):
+        '''Removes node id_.'''
+        id_ = self._extract_uuid(id_)
+        if self._g.has_node(id_):
+            # for DiGraph, remove predecessors AND successors
+            for successor in self._g.neighbors(id_):
+                # need to iterate over items() (which copies the dict) because we are
+                # removing items from the edges dict as we are iterating over it
+                for edge in self._g.edge[id_][successor].items():
+                    self.remove_edge(self._g.edge[id_][successor][edge[0]]["id"]) # edge[0] is the edge's ID
+            for predecessor in self._g.predecessors(id_):
+                for edge in self._g.edge[predecessor][id_].items():
+                    self.remove_edge(self._g.edge[predecessor][id_][edge[0]]["id"])
+
+            self._g.remove_node(id_)
+        else:
+            raise GraphException("Node ID not found.")
--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from Graph import *
+from DiGraph import *
