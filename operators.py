6ef682d8acdce1e6ead808eda9eaf9ebbfe2b7f9
skyler <skyler@opendns.com>
2014-07-01 12:14:18 -0700

    Allow networkx graphs to be passed into operators for chaining.

--- operators.py
+++ operators.py
@@ -1,10 +1,19 @@
 import networkx as nx
 import semanticnet as sn
 
+def _extract_networkx_graph(G):
+    mod = G.__module__.split('.')[0]
+    if mod == 'semanticnet':
+        return G.networkx_graph()
+    elif mod == 'networkx':
+        return G
+    else:
+        raise GraphException("Invalid graph object. Must be from networkx or semanticnet.")
+
 def difference(A, B):
     '''Returns a new graph which contains the nodes and edges in A, but not in B.'''
-    A = A.networkx_graph()
-    B = B.networkx_graph()
+    A = _extract_networkx_graph(A)
+    B = _extract_networkx_graph(B)
     C = A.copy()
     C.remove_nodes_from(n for n in A.nodes() if n in B.nodes())
     C.remove_edges_from(
