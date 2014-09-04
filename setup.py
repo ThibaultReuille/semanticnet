8c93a890237e3676a17f07f377686f20aa18e13f
skyler <skyler@opendns.com>
2014-09-04 15:30:14 -0700

    setup.py: bump version
    Also, fix setup.py and remove requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 
-import os
 from setuptools import setup
-
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
 
 setup(
     name='semanticnet',
     packages=['semanticnet'],
-    version='0.1.0',
+    version='0.2.0',
     description='Small python library to create semantic graphs in JSON.',
     author='Thibault Reuille',
     author_email='thibault@opendns.com',
     url="https://github.com/ThibaultReuille/semanticnet",
-    install_requires=required
+    install_requires=['networkx']
 )
